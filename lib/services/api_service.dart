8d06d9507fe97210664d6ac6ba220b230c711543
Diego Arturo Yangua Merino <103982003+Dacyz@users.noreply.github.com>
2023-11-28 10:55:56 -0500

    upd

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 class ApiService {
   static const String baseUrl =
-      "http://192.168.43.65/api_flutter"; // IP para acceder a localhost desde un emulador Android : Olga
+      //"http://192.168.43.65/api_flutter"; // IP para acceder a localhost desde un emulador Android : Olga
       //"http://192.168.18.6/api_flutter"; // IP para acceder a localhost desde un emulador Android : Diego
+      "https://ucvpppix.net/api_flutter"; // IP para acceder a localhost desde un emulador Android : PROD
 }
